b98c3f27ec8f4d6f99c3e618d1d34577126f7225
Beyond-Stars-ai <3220852648@qq.com>
2026-09-10 20:52:23 +0800

    原解决方式有保留必要

--- Progame_IV/Progame_IV.c
+++ Progame_IV/Progame_IV.c
@@ -5,6 +5,7 @@
  */
 
 #include "pico/stdlib.h"
+#include "hardware/i2c.h"
 #include "stdio.h"
 
 // Pico W devices use a GPIO on the WIFI chip for the LED,
@@ -14,8 +15,15 @@
 #endif
 
 #ifndef LED_DELAY_MS
-#define LED_DELAY_MS 1000
+#define LED_DELAY_MS 500
 #endif
+
+// I2C defines
+// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define I2C_PORT i2c0
+#define I2C_SDA 8
+#define I2C_SCL 9
 
 // Perform initialisation
 int pico_led_init(void) {
@@ -45,6 +53,15 @@ void pico_set_led(bool led_on) {
 int main() {
     // 初始化标准输入输出（USB 或 UART） 
     stdio_init_all();
+
+    // I2C Initialisation. Using it at 400Khz.
+    i2c_init(I2C_PORT, 400*1000);
+    
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
     int rc = pico_led_init();
     hard_assert(rc == PICO_OK);
